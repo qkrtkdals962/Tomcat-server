004c15fbd3b58f7a818784b26cfb8b0b94197447
HyeonJin127 <guswls041207@naver.com>
2025-05-09 22:12:15 +0900

    Update BoardController.java

--- board/src/main/java/com/diet/board/controller/BoardController.java
+++ board/src/main/java/com/diet/board/controller/BoardController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/posts")
+@RequestMapping("/posts")
 public class BoardController {
     private final PostRepository postRepository;
 
